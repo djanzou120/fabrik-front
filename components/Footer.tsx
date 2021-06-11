532f9905da4cdd40d73fc967aade893ac9294a5d
Philippe <Philimart21@gmail.com>
2021-06-11 11:18:38 +0100

    initial commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="flex justify-center">
         <div className="flex flex-col w-80 sm:w-3/5 mt-52">
           <div className="text-gray-400 text-2xl font-medium"> Result</div>
-          <div className="text-gray-100"></div>
+          <div className="text-gray-100" />
         </div>
       </div>
       <div className="text-gray-100 px-5 py-1 flex justify-end h-1/12 absolute bottom-0 w-full">
--- components/Main.tsx
+++ components/Main.tsx
@@ -1,9 +1,34 @@
 import React from "react";
+import Tabs, { TabPane } from "rc-tabs";
+import { MdSecurity } from "react-icons/md";
+import { BsFilePost } from "react-icons/bs";
 
 const Main = () => {
   return (
     <div className="bg-white shadow-lg bg-gray-50 rounded-md w-80 sm:w-3/5 h-96 absolute top-60 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      TEXT
+      <Tabs className="option-modal-tabs" tabPosition="left" tabBarGutter={16}>
+        <TabPane
+          tab={
+            <div className="flex border">
+              <MdSecurity size={27} className="mr-2" />
+              Authentication
+            </div>
+          }
+          key="authentication"
+        >
+          first
+        </TabPane>
+        <TabPane
+          tab={
+            <div className="flex border">
+              <BsFilePost size={27} className="mr-2" /> Post
+            </div>
+          }
+          key="post"
+        >
+          second
+        </TabPane>
+      </Tabs>
     </div>
   );
 };
