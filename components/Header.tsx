9ef00f7ea702aacd07ee725d2c9af4db8defbfa6
Philippe <Philimart21@gmail.com>
2021-06-11 11:25:12 +0100

    initial commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="h-2/6 w-full bg-gradient-to-tr from-blue-500 to-blue-600" />
+    <div className="h-2/6 w-full bg-gradient-to-tr from-blue-400 to-blue-500" />
   );
 };
 
--- components/Main.tsx
+++ components/Main.tsx
@@ -5,7 +5,7 @@ import { BsFilePost } from "react-icons/bs";
 
 const Main = () => {
   return (
-    <div className="bg-white shadow-lg bg-gray-50 rounded-md w-80 sm:w-3/5 h-96 absolute top-60 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+    <div className="bg-black text-gray-200 border-2 shadow-lg rounded-xl w-80 sm:w-3/5 h-96 absolute top-60 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
       <Tabs className="option-modal-tabs" tabPosition="left" tabBarGutter={16}>
         <TabPane
           tab={
