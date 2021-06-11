f57e48f15aff6c699de0d7c85628e64336cd7b39
Philippe <Philimart21@gmail.com>
2021-06-11 11:05:40 +0100

    initial commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,10 @@ const Footer = () => {
   return (
     <div className="h-4/6 w-full bg-black">
       <div className="flex justify-center">
-        <div className="text-gray-100 w-80 sm:w-3/5 mt-52">Result</div>
+        <div className="flex flex-col w-80 sm:w-3/5 mt-52">
+          <div className="text-gray-400 text-2xl font-medium"> Result</div>
+          <div className="text-gray-100"></div>
+        </div>
       </div>
       <div className="text-gray-100 px-5 py-1 flex justify-end h-1/12 absolute bottom-0 w-full">
         Github
