85b72b3dac5de8d784ac1b1430ab832a33b67416
Philippe <Philimart21@gmail.com>
2021-06-11 11:03:03 +0100

    initial commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,10 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="h-4/6 w-full bg-black">
-      <div className="border flex justify-center">
-        <div className="text-gray-100 border w-80 sm:w-3/5 mt-52">result</div>
+      <div className="flex justify-center">
+        <div className="text-gray-100 w-80 sm:w-3/5 mt-52">Result</div>
       </div>
-
       <div className="text-gray-100 px-5 py-1 flex justify-end h-1/12 absolute bottom-0 w-full">
         Github
       </div>
