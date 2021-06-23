42b134f1d1cac62fa8dc5b0a9290df464d5817c6
Philippe <Philimart21@gmail.com>
2021-06-23 11:19:14 +0100

    up

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import React from "react";
-import Main from "../components/Main";
+import Tabulations from "../components/Tabulations";
 
 export default function Home() {
   return (
@@ -14,7 +14,9 @@ export default function Home() {
       </Head>
       <div className="h-screen flex flex-col w-full">
         <Header />
-        <Main />
+        <main className="bg-black px-3 text-gray-200 border-4 border-gray-800 shadow-lg rounded-2xl w-80 sm:w-3/5 h-96 absolute top-60 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <Tabulations />
+        </main>
         <Footer />
       </div>
     </div>
